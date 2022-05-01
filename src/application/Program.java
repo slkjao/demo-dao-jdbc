f99f02ff2496bc5ab5ed274def509bf5429168b8
joao costa <joaovaraujoc@gmail.com>
2022-05-01 09:25:14 -0300

    seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Department book = new Department(1,"book");
+		Seller seller = new Seller(2, "joao", "joao@gmail", new Date(), 2000.0, book);
+		System.out.println(seller.getName());
 	}
 
 }
