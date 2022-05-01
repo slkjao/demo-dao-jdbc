2d7f29a1d83dfe09323783eec6a1ce1899cdc244
joao costa <joaovaraujoc@gmail.com>
2022-05-01 09:49:25 -0300

    daoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,9 @@ public class Program {
 		// TODO Auto-generated method stub
 		Department book = new Department(1,"book");
 		Seller seller = new Seller(2, "joao", "joao@gmail", new Date(), 2000.0, book);
+		SellerDao sd = DaoFactory.createSellerDao();
+		
+		
 		System.out.println(seller.getName());
 	}
 
